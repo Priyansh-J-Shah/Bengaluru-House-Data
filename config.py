f04c2caad39417eddc27d0ea77744e8282301b66
Priyansh-J-Shah <spriyansh78@gmail.com>
2022-10-27 14:03:05 +0530

    Project Configuration

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+Bengaluru_CSV_FILE_PATH = os.path.join("Model","Bengaluru_House_Data.csv")
+
+Bengaluru_MODEL_FILE_PATH = os.path.join("Model","Linear_Regression_Bengaluru_House_Data.pkl")
+
+Bengaluru_JSON_FILE_PATH = os.path.join("Model","Linear_Regression_Bengaluru_House_Data.json")
+
+PORT_NUMBER = 5700
